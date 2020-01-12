be44bb7046b8d43c45b05812e0484f6e90bc9c01
Torsten Krause <tk@markenwerk.net>
2020-01-12 13:27:24 +0100

    Add a type-based audit delegate that calls consumers

---
+++ module/core/src/main/java/io/markdom/handler/ConsumingTypebasedMarkdomAuditDelegate.java
@@ -0,0 +1,31 @@
+package io.markdom.handler;
+
+import java.util.function.Consumer;
+
+import io.markdom.common.MarkdomBlockType;
+import io.markdom.common.MarkdomContentType;
+import io.markdom.util.ObjectHelper;
+
+public final class ConsumingTypebasedMarkdomAuditDelegate implements TypebasedMarkdomAuditDelegate {
+
+	private final Consumer<MarkdomBlockType> blockTypesConsumer;
+
+	private final Consumer<MarkdomContentType> contentTypeConsumer;
+
+	public ConsumingTypebasedMarkdomAuditDelegate(Consumer<MarkdomBlockType> blockTypesConsumer,
+			Consumer<MarkdomContentType> contentTypeConsumer) {
+		this.blockTypesConsumer = ObjectHelper.notNull("block type consumer", blockTypesConsumer);
+		this.contentTypeConsumer = ObjectHelper.notNull("content type consumer", contentTypeConsumer);
+	}
+
+	@Override
+	public void onBlockType(MarkdomBlockType type) {
+		blockTypesConsumer.accept(type);
+	}
+
+	@Override
+	public void onContentType(MarkdomContentType type) {
+		contentTypeConsumer.accept(type);
+	}
+
+}
